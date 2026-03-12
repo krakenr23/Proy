17d70f1ab1a06e8db1f6050dc2b97bce8c58c20c
krakenr23 <146369538+krakenr23@users.noreply.github.com>
2026-03-12 09:32:09 -0600

    Add files via upload

---
+++ gpt.h
@@ -0,0 +1,44 @@
+#include <stdint.h>
+
+// No hagas cosas raras (en aras de optimizar)
+
+#pragma pack(push,1)
+
+struct gpt_guid
+{
+	uint8_t uuid[16];
+};
+
+struct gpt_header {
+	uint64_t            signature; /* header identification */
+	uint32_t            revision; /* header version */
+	uint32_t            size; /* in bytes */
+	uint32_t            crc32; /* header CRC checksum */
+	uint32_t            reserved1; /* must be 0 */
+	uint64_t            my_lba; /* LBA of block that contains this struct (LBA 1) */
+	uint64_t            alternative_lba; /* backup GPT header */
+	uint64_t            first_usable_lba; /* first usable logical block for partitions */
+	uint64_t            last_usable_lba; /* last usable logical block for partitions */
+	struct gpt_guid 	disk_guid; /* unique disk identifier */
+	uint64_t            partition_entry_lba; /* LBA of start of partition entries array */
+	uint32_t            npartition_entries; /* total partition entries - normally 128 */
+	uint32_t            sizeof_partition_entry; /* bytes for each GUID pt */
+	uint32_t            partition_entry_array_crc32; /* partition CRC checksum */
+	uint8_t             reserved2[512 - 92]; /* must all be 0 */
+};
+
+///
+/// GPT Partition Entry.
+///
+typedef struct
+{
+	struct gpt_guid PartitionTypeGUID;
+	struct gpt_guid UniquePartitionGUID;
+  	uint64_t 	    start;
+  	uint64_t 	    end;
+  	uint64_t 	    attrib;
+	uint16_t        PartitionName[36]; // Nombre en UTF16
+} efi_partition_entry;
+
+
+#pragma pack(pop)
---
+++ leeMBR.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <string.h>
+#include <curses.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+#include "mbr.h"
+#include "gpt.h"
+
+char *mapFile(char *filePath) {
+    /* Abre archivo */
+    int fd = open(filePath, O_RDONLY);
+    if (fd == -1) {
+    	perror("Error abriendo el archivo");
+	    return(NULL);
+    }
+
+    /* Mapea archivo */
+    struct stat st;
+    fstat(fd,&st);
+    long fs = st.st_size;
+
+    char *map = mmap(0, fs, PROT_READ, MAP_SHARED, fd, 0);
+    if (map == MAP_FAILED) {
+    	close(fd);
+	    perror("Error mapeando el archivo");
+	    return(NULL);
+    }
+
+  return map;
+}
+
+void desp_part(int sel, MBR_PARTITION_RECORD p[]) {
+    for (int j=0; j < 4; j++) {
+         if (j == sel) {
+           attron(A_REVERSE);
+         }
+         // Despliega información de la partición
+         mvprintw(5+j,5,"%5x     %7d     %7d",
+            p[j].OSIndicator,
+            p[j].StartingLBA,
+            p[j].SizeInLBA
+        );
+         attroff(A_REVERSE);
+      }
+      move(5+sel,5);
+      refresh();
+}
+
+int main()
+{
+   char *lista[] = {"Uno", "Dos", "Tres", "Cuatro" };
+   int i = 0;
+   int c;
+   
+   // Mapea imagen del disco
+   char *base = mapFile("DiscoAPFS.dmg");
+
+   initscr();
+   raw();
+   keypad(stdscr, TRUE); /* Habilita las teclas de funcion y flechas */
+   noecho(); /* No muestres el caracter leido */
+   cbreak(); /* Haz que los caracteres se le pasen al usuario */
+   
+   // Leer MBR
+   MASTER_BOOT_RECORD mbr;
+   memcpy(&mbr,base,sizeof(mbr)); // Aqui se puede hacer un cast directo, pero lo hago con memcpy para evitar warnings de alignment
+   desp_part(0,mbr.Partition);
+
+   do {
+      c = getch();
+      switch(c) {
+         case KEY_UP:
+            i = (i>0) ? i - 1 : 3;
+            break;
+         case KEY_DOWN:
+            i = (i<3) ? i + 1 : 0;
+            break;
+         default:
+            // Nothing 
+            break;
+      }
+      move(10,5);
+      printw("Estoy en %d: Lei %d",i,c);
+   } while (c != 'q');
+   endwin();
+   return 0;
+}
